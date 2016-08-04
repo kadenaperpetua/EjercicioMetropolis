3b86fb4a9ac9f003a9b7560ca066c52f05edbefb
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-04 18:18:24 -0500

    ELI: first commit

---
+++ src/com/eli/metropolis/Java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by elizabeth on 8/3/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int entero = devuelveEntero();
+        System.out.println(entero);
+    }
+
+    private static Integer devuelveEntero(){
+        Integer entero=0;
+        entero.
+       return entero;
+    }
+
+
+}
