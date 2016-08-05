cfbea3cf52f8a48138cc6ee410dbb029d33cd1dd
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-04 19:02:25 -0500

    ELI: escribir funcion final

---
+++ src/com/eli/metropolis/Java/Metropolis.java
@@ -0,0 +1,26 @@
+/**
+ * Created by elizabeth on 8/3/16.
+ */
+public class Metropolis {
+    public static void main(String[] args) {
+        boolean esCapital =true;
+        int numeroDeCiudadanos =200000;
+        double impuestosPorCiudadano = 800.0;
+        if (esMetropolis(esCapital, numeroDeCiudadanos, impuestosPorCiudadano)){
+            System.out.println("Si es una Metropolis");
+        } else {
+            System.out.println("No es una Metropolis");
+        }
+    }
+
+    private static boolean esMetropolis(boolean esCapital, int numeroDeCiudadanos, double impuestosPorCiudadano){
+        double ingresoPromedioAnio = numeroDeCiudadanos*impuestosPorCiudadano*12;
+
+        if( ingresoPromedioAnio>720000000 &&  numeroDeCiudadanos>=200000 || esCapital && numeroDeCiudadanos>=100000){
+            return true;
+        }
+
+        return false;
+    }
+
+}
