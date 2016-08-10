d4d45c6f75203cda158b4bba3b30bae97f36b25a
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-10 12:13:40 -0500

    ELI: cambia el nombre del metodo esCapital por verificarSiEsCapital

--- src/com/eli/metropolis/Java/Metropolis.java
+++ src/com/eli/metropolis/Java/Metropolis.java
@@ -3,20 +3,21 @@
  */
 public class Metropolis {
     public static void main(String[] args) {
-        boolean esCapital =true;
-        int numeroDeCiudadanos =200000;
+        boolean esCapital = true;
+        int numeroDeCiudadanos = 200000;
         double impuestosPorCiudadano = 800.0;
-        if (esMetropolis(esCapital, numeroDeCiudadanos, impuestosPorCiudadano)){
+
+        if (verificarSiEsMetropolis(esCapital, numeroDeCiudadanos, impuestosPorCiudadano)) {
             System.out.println("Si es una Metropolis");
         } else {
             System.out.println("No es una Metropolis");
         }
     }
 
-    private static boolean esMetropolis(boolean esCapital, int numeroDeCiudadanos, double impuestosPorCiudadano){
-        double ingresoPromedioAnio = numeroDeCiudadanos*impuestosPorCiudadano*12;
+    private static boolean verificarSiEsMetropolis(boolean esCapital, int numeroDeCiudadanos, double impuestosPorCiudadano) {
+        double ingresoPromedioAnio = numeroDeCiudadanos * impuestosPorCiudadano * 12;
 
-        if( ingresoPromedioAnio>720000000 &&  numeroDeCiudadanos>=200000 || esCapital && numeroDeCiudadanos>=100000){
+        if (ingresoPromedioAnio > 720000000 && numeroDeCiudadanos >= 200000 || (esCapital && numeroDeCiudadanos >= 100000)) {
             return true;
         }
 
